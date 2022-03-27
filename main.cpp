84e4d556aea0d10bfac777d1c5cb510772ea7f8c
hxx0224 <95512868+hxx0224@users.noreply.github.com>
2022-03-27 19:34:18 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,8 +8,8 @@ using namespace std;
  
 int main()
 {
-
-	printf("%lf \n", t_sin(1.57));//调取sct.h中t_sin函数计算正弦函数值 
+    //调取sct.h中函数计算函数值
+	printf("%lf \n", t_sin(1.22)); 
 	printf("%lf \n", t_cos(0.52));	 
     printf("%lf \n", t_tan(3.14));
     printf("%lf \n", t_sec(3.14));	 
@@ -17,6 +17,15 @@ int main()
 	printf("%lf \n", t_cot(1.57));
 	printf("%lf \n", deg2rad(30));
 	printf("%lf \n", rad2deg(1.57));
-    
+	printf("%lf\n", t_acos(0.99999988888887));
+	printf("%lf\n", t_asin(0.1));
+	printf("%lf\n", t_atan(20));
+	//调取math.h中函数计算函数值
+	printf("%lf \n", sin(1.22)); 
+	printf("%lf \n", cos(0.52));	 
+	printf("%lf \n", tan(3.14));
+	printf("%lf\n", acos(0.99999988888887));
+	printf("%lf\n", asin(0.1));
+	printf("%lf\n", atan(20));     
 	return 0;
 }
--- sct.h
+++ sct.h
@@ -15,16 +15,26 @@ double deg2rad(double deg)
 {
     return (deg*PI)/180.0;
 }
+//弧度转角度 
 double rad2deg(double rad)
 {
     return rad*180/PI;
 }
  
-double fabs(double x)
-{
-	if (x < 0)
-		return -x;
-	return x;
+//牛顿迭代法求开方
+double k_sqrt(double a,double x0)
+{ 
+	
+	double x1,y;
+	x1=(x0+a/x0)/2.0;  
+	if(fabs(x1-x0)>=10e-15)
+		y = k_sqrt(a,x1);
+	else  y=x1;
+  return y;
+}
+double sqrt(double x)
+{ 
+	return k_sqrt(x, 1.0);
 }
  
 //区别于库函数的math.h中sin() 函数，所以采用t_sin()表示 
@@ -60,15 +70,81 @@ double t_sec(double x)
 {
 	return 1.0/t_cos(x);
 }
-
+//t_csc 
 double t_csc(double x)
 {
 	return 1.0/t_sin(x);
 }
-
+//t_cot
 double t_cot(double x)
 {
 	return t_cos(x)/t_sin(x);
 }
+//x^n值 
+double pow(double x, int n)
+{
+	double result = 1;
+	if (n == 0)
+		return 1;
+	for (int i = 0; i < n; i++)
+	{
+		result*=x;
+	}
+ 
+	return result;
+}
+ //x!阶乘 
+long factorial(int x)
+{
+	long result = 1;
+	for(int i = 1; i <= x; i++)
+		result *= i;
+ 
+	return result;
+}
+//arcsin 
+double t_asin(double x)
+{	
+	double a0=1.5707288,a1=-0.2121144,a2=0.0742610,a3 = -0.0187293;
+	return	(PI/2 - sqrt(1 - x)*(a0 + a1*x + a2*pow(x,2) + a3*pow(x,3)));
+	
+}
+ 
+ //arccos 
+double t_acos(double x)
+{
+ 
+	if (fabs(x) < 0.5)
+		return PI / 2 - t_asin(x);
+	else
+		return 2 * t_asin(sqrt((1-x)/2));
+ 
+}
+//t_atan
+double t_atan( double x )
+{
+    double result;
+	if(x==0)
+	{
+		result=0.0;
+	}
+	else if(0<x && x<1)
+	{
+		result= x/(1+0.28086*pow(x,2));
+	}
+	else if(x>=1)
+	{
+		result= PI/2-t_atan(1/x);
+	}
+	else if(x<0)
+	{
+		result= -t_atan(-x);
+	}
+	return result;
+}
+ 
+ 
+
+
 
 #endif
