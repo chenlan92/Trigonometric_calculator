016b7d88ca15caea8c7c54c2c10e8d7bc7891ac3
Fanjl23 <101792523+Fanjl23@users.noreply.github.com>
2022-03-30 23:13:28 +0800

    Create mysin.cpp

---
+++ mysin.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<math.h>
+double mysin(double x)
+{
+	int n;
+	int sign;
+	double x, res, item;
+	const double epsilon = 0.0001;
+	res = x;
+	item = x * x * x / 6;
+	sign = -1;
+	n = 2;
+	while (item > epsilon)
+	{
+		res = res + item * sign;
+		item = item * x * x / ((2 * n) * (2 * n + 1));
+		sign = -sign;
+		n++;
+	}
+	return res;
+}
