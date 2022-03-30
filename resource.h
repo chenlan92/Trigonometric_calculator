5472e4311872dcbab5f5a5b05f2fbd72275746e9
chenlan92 <101275643+chenlan92@users.noreply.github.com>
2022-03-30 20:19:03 +0800

    Add files via upload

--- resource.h
+++ resource.h
@@ -1,44 +1,35 @@
 ﻿//{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ 生成的包含文件。
-// 供 MFC1.rc 使用
+// 供 Trigonometriccalculator.rc 使用
 //
 #define IDM_ABOUTBOX                    0x0010
 #define IDD_ABOUTBOX                    100
 #define IDS_ABOUTBOX                    101
-#define IDD_MFC_1_DIALOG                102
+#define IDD_TRIGONOMETRIC_CALCULATOR_DIALOG 102
 #define IDR_MAINFRAME                   128
 #define IDC_NUM1                        1000
-#define IDC_NUM2                        1001
-#define IDC_NUM3                        1002
-#define IDC_NUM4                        1003
-#define IDC_NUM5                        1004
-#define IDC_NUM6                        1005
-#define IDC_NUM7                        1006
-#define IDC_NUM8                        1007
-#define IDC_NUM9                        1008
-#define IDC_NUM0                        1009
-#define IDC_DOT                         1010
-#define IDC_SIGN                        1011
-#define IDC_SIN                         1012
-#define IDC_COS                         1013
-#define IDC_TAN                         1014
-#define IDC_SEC                         1015
-#define IDC_COT                         1016
-#define IDC_CSC                         1017
-#define IDC_ADD                         1018
-#define IDC_SUB                         1019
-#define IDC_MUL                         1020
-#define IDC_DIV                         1021
-#define IDC_RAD                         1023
-#define IDC_ANG                         1024
-#define IDC_EQU                         1025
+#define IDC_ANGLE                       1004
+#define IDC_SHOW                        1006
+#define IDC_DEL                         1008
+#define IDC_NUM4                        1011
+#define IDC_NUM7                        1012
+#define IDC_SIGN                        1013
+#define IDC_NUM2                        1014
+#define IDC_NUM5                        1015
+#define IDC_NUM8                        1016
+#define IDC_NUM0                        1017
+#define IDC_NUM3                        1018
+#define IDC_NUM6                        1019
+#define IDC_NUM9                        1020
+#define IDC_DOT                         1021
+#define IDC_SIN                         1022
+#define IDC_COS                         1023
+#define IDC_ARCSIN                      1024
+#define IDC_ARCTAN                      1025
 #define IDC_AC                          1026
-#define IDC_OUTPUT                      1027
-#define IDC_DEL                         1028
-#define IDC_ANS                         1029
-#define IDC_INPUT                       1030
-#define IDC_OPERATION                   1031
-#define IDC_SAVE                        1032
+#define IDC_TYPE                        1027
+#define IDC_SHOWTYPE                    1028
+#define IDC_ERROR                       1029
 
 // Next default values for new objects
 // 
@@ -46,7 +37,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1032
+#define _APS_NEXT_CONTROL_VALUE         1029
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
